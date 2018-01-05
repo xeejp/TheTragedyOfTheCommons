0c16d880c87095113b0a1b820bc6f81d8fbcdfa9
Ryohei HAYASHI <hayashi@xee.jp>
2018-01-06 00:27:48 +0900

    change dependencies

--- mix.exs
+++ mix.exs
@@ -19,8 +19,8 @@ defmodule TragedyOfTheCommons.Mixfile do
   end
 
   defp deps do
-    [{:xeethemescript, "~> 0.1.0"},
-     {:json_diff_ex, github: "ryo33/json_diff_ex", branch: "fix-array-diff"},
-     {:transmap, "~> 0.2.0"}]
+    [{:xeethemescript, "~> 0.3.0"},
+     {:json_diff_ex, "~> 0.6.0"},
+     {:transmap, "~> 0.4.0"}]
   end
 end
